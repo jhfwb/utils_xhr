0e7de0bae6ebdd84610658a041bf867799e98b85
xmind <527077832@qq.com>
2021-08-09 12:01:19 +0800

    基础工具包发布

--- demos.py
+++ demos.py
@@ -1,7 +1,7 @@
+import pyPacking_xhr
+
 from utils_xhr.io import replaceFileSign
 from utils_xhr.reflex.reflexUtils import ReflexUtils
 from utils_xhr.reflex import reflexUtils as aaa
+# pyPacking_xhr.PackingTool().create_packing_env(pyPackage='utils_xhr')
 
-# ReflexUtils().getFuncByDecoratorName(aaa,'@threadingRun')[0]()
-
-replaceFileSign(path='test.py',signs=[('@a@','你好')])
